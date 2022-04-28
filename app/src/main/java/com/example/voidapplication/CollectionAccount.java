019bd04bd1aef82f3c1580940eba9e191d4a092c
Sapino911 <letsapamasego@gmail.com>
2022-04-29 00:56:28 +0200

    "Firebase Collections Logout Button"

--- app/src/main/java/com/example/voidapplication/CollectionAccount.java
+++ app/src/main/java/com/example/voidapplication/CollectionAccount.java
@@ -2,12 +2,17 @@ package com.example.voidapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CollectionAccount extends AppCompatActivity {
     private ImageButton btncirlceadd;
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,16 @@ public class CollectionAccount extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 openAdd_Collection();
+            }
+        });
+
+        logout = (Button) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(CollectionAccount.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
